b78d3716617cb9cce87294d80508a78c6e0ea106
Dianhanluosi <evanzheng1127@gmail.com>
2024-03-07 18:10:01 -0500

    .

--- Source/Subkronica/PlayerCharacter.cpp
+++ Source/Subkronica/PlayerCharacter.cpp
@@ -11,8 +11,8 @@ APlayerCharacter::APlayerCharacter()
 	PrimaryActorTick.bCanEverTick = true;
 
 	// Initialize Follow Camera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(RootComponent); // Attach the camera to the end of the swing arm
+	Cam = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	Cam->SetupAttachment(RootComponent); // Attach the camera to the end of the swing arm
 
 }
 
@@ -20,6 +20,8 @@ APlayerCharacter::APlayerCharacter()
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	MoveSpeed = WalkingSpeed;
 	
 }
 
@@ -35,5 +37,56 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(*MoveForward, this, &APlayerCharacter::MoveFowardCtrl);
+	PlayerInputComponent->BindAxis(*MoveRight, this, &APlayerCharacter::MoveRightCtrl);
+	PlayerInputComponent->BindAxis(*Sprint, this, &APlayerCharacter::SprintCtrl);
+
+	PlayerInputComponent->BindAxis(*LookUp, this, &APlayerCharacter::LookUpCtrl);
+	PlayerInputComponent->BindAxis(*TurnRight, this, &APlayerCharacter::TurnRightCtrl);
+
+	PlayerInputComponent->BindAction(*Jumping, EInputEvent::IE_Pressed, this, &ACharacter::Jump);
 }
+
+void APlayerCharacter::MoveFowardCtrl(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue * MoveSpeed * GetWorld()->GetDeltaSeconds());
+	UE_LOG(LogTemp, Log, TEXT("MoveSpeed: %f"), MoveSpeed);
+}
+
+void APlayerCharacter::MoveRightCtrl(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue * MoveSpeed * GetWorld()->GetDeltaSeconds());
+	UE_LOG(LogTemp, Log, TEXT("MoveSpeed: %f"), MoveSpeed);
+}
+
+void APlayerCharacter::SprintCtrl(float AxisValue)
+{
+	if (AxisValue == 1)
+	{
+		MoveSpeed = RunningSpeed;
+	}
+	else
+	{
+		MoveSpeed = WalkingSpeed;
+	}
+}
+
+
+void APlayerCharacter::LookUpCtrl(float AxisValue)
+{
+	if (Cam)
+	{
+		FRotator NewRotation = Cam->GetRelativeRotation();
+		NewRotation.Pitch = FMath::Clamp(NewRotation.Pitch + AxisValue * RotationRateUp * GetWorld()->GetDeltaSeconds(), MinPitchAngle, MaxPitchAngle);
+		Cam->SetRelativeRotation(NewRotation);
+	}
+	
+	//AddControllerPitchInput(AxisValue * RotationRate * GetWorld()->GetDeltaSeconds());
+}
+
+void APlayerCharacter::TurnRightCtrl(float AxisValue)
+{
+	AddControllerYawInput(AxisValue * RotationRateRight * GetWorld()->GetDeltaSeconds());
+}
+
 
--- Source/Subkronica/PlayerCharacter.h
+++ Source/Subkronica/PlayerCharacter.h
@@ -26,7 +26,67 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	//Set up camera component
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
-	class UCameraComponent* FollowCamera;
+	class UCameraComponent* Cam;
+
+	//Set up control keywords
+	//Axis
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	FString MoveForward = "MoveForward";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	FString MoveRight = "MoveRight";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	FString LookUp = "LookUp";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	FString TurnRight = "TurnRight";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	FString Sprint = "Sprint";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	FString Hold = "Hold";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	FString Pee = "Pee";
+	
+	//Actions
+	UPROPERTY(EditDefaultsOnly, Category = "Control Action")
+	FString Crouching = "Crouch";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Action")
+	FString Jumping = "Jump";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Action")
+	FString Click = "Click";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Action")
+	FString RightClick = "RightClick";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Action")
+	FString Scroll = "Scroll";
+	UPROPERTY(EditDefaultsOnly, Category = "Control Action")
+	FString Collect = "Collect";
+	
+private:
+	//Set up movement
+	void MoveFowardCtrl (float AxisValue);
+	void MoveRightCtrl (float AxisValue);
+	void SprintCtrl (float AxisValue);
+
+	//Set up camera movement
+	void LookUpCtrl (float AxisValue);
+	void TurnRightCtrl (float AxisValue);
+
+	//Set up movement rate
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	float RunningSpeed = 500;
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	float WalkingSpeed = 200;
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	float MoveSpeed = 100;
+	
+	//Set up camera rate
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	float RotationRateRight = 10;
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	float RotationRateUp = 50;
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	float MinPitchAngle = -85;
+	UPROPERTY(EditDefaultsOnly, Category = "Control Axis")
+	float MaxPitchAngle = 85;
 	
 };
